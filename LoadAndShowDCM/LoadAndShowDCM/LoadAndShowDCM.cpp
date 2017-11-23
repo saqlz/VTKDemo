96a8f2d9b2da8d422bb9f07250f4e32c73a3df3e
qinglin.zhang <qinglin.zhang@DESKTOP-LB4U4L7>
2017-11-23 10:03:18 +0800

    tijiao

--- LoadAndShowDCM/LoadAndShowDCM/LoadAndShowDCM.cpp
+++ LoadAndShowDCM/LoadAndShowDCM/LoadAndShowDCM.cpp
@@ -43,6 +43,24 @@ PURPOSE.  See the above copyright notice for more information.
 #include "vtkImageMapper3D.h"
 #include "vtkStreamingDemandDrivenPipeline.h"
 #include "vtkInformation.h"
+#include "vtkImageGridSource.h"
+#include "vtkImageBSplineInterpolator.h"
+#include "vtkImageBSplineCoefficients.h"
+#include "vtkImageReslice.h"
+#include "vtkBSplineTransform.h"
+#include "vtkTransformToGrid.h"
+#include "vtkThinPlateSplineTransform.h"
+#include "vtkImageViewer.h"
+#include "vtkImageGridSource.h"
+#include "vtkImageBlend.h"
+#include "vtkImageMapToColors.h"
+#include "vtkLookupTable.h"
+#include "vtkPoints.h"
+#include "vtkRenderer.h"
+#include "vtkRenderWindow.h"
+#include "vtkRenderWindowInteractor.h"
+#include "vtkRegressionTestImage.h"
+#include "vtkSmartPointer.h"
 
 namespace
 {
@@ -64,141 +82,25 @@ namespace
     }
 }
 
-// The mouse motion callback, to turn "Slicing" on and off
-class vtkImageInteractionCallback : public vtkCommand
-{
-public:
-
-    static vtkImageInteractionCallback *New() {
-        return new vtkImageInteractionCallback;
-    };
-
-    vtkImageInteractionCallback() {
-        this->Slicing = 0;
-        this->ImageReslice = 0;
-        this->Interactor = 0;
-    };
-
-    void SetImageReslice(vtkImageReslice *reslice) {
-        this->ImageReslice = reslice;
-    };
-
-    vtkImageReslice *GetImageReslice() {
-        return this->ImageReslice;
-    };
-
-    void SetInteractor(vtkRenderWindowInteractor *interactor) {
-        this->Interactor = interactor;
-    };
-
-    vtkRenderWindowInteractor *GetInteractor() {
-        return this->Interactor;
-    };
-
-    void Execute(vtkObject *, unsigned long event, void *) VTK_OVERRIDE
-    {
-        vtkRenderWindowInteractor *interactor = this->GetInteractor();
-
-        int lastPos[2];
-        interactor->GetLastEventPosition(lastPos);
-        int currPos[2];
-        interactor->GetEventPosition(currPos);
-
-        if (event == vtkCommand::LeftButtonPressEvent)
-        {
-            this->Slicing = 1;
-        }
-        else if (event == vtkCommand::LeftButtonReleaseEvent)
-        {
-            this->Slicing = 0;
-        }
-        else if (event == vtkCommand::MouseMoveEvent)
-        {
-            if (this->Slicing)
-            {
-                vtkImageReslice *reslice = this->ImageReslice;
-
-                // Increment slice position by deltaY of mouse
-                int deltaY = lastPos[1] - currPos[1];
-
-                reslice->Update();
-                double sliceSpacing = reslice->GetOutput()->GetSpacing()[2];
-                vtkMatrix4x4 *matrix = reslice->GetResliceAxes();
-                // move the center point that we are slicing through
-                double point[4];
-                double center[4];
-                point[0] = 0.0;
-                point[1] = 0.0;
-                point[2] = sliceSpacing * deltaY;
-                point[3] = 1.0;
-                matrix->MultiplyPoint(point, center);
-                matrix->SetElement(0, 3, center[0]);
-                matrix->SetElement(1, 3, center[1]);
-                matrix->SetElement(2, 3, center[2]);
-                interactor->Render();
-            }
-            else
-            {
-                vtkInteractorStyle *style = vtkInteractorStyle::SafeDownCast(
-                    interactor->GetInteractorStyle());
-                if (style)
-                {
-                    style->OnMouseMove();
-                }
-            }
-        }
-    };
-
-private:
-
-    // Actions (slicing only, for now)
-    int Slicing;
-
-    // Pointer to vtkImageReslice
-    vtkImageReslice *ImageReslice;
-
-    // Pointer to the interactor
-    vtkRenderWindowInteractor *Interactor;
-};
-
-
-
-// The program entry point
 int main()
 {
-    std::cout << sizeof(float) << std::endl;
     std::string fDoseFilePath = "D:\\GitHub\\WisdomRay\\appdata\\dose.dat";
-    int iVolumeDimension[3] = {150, 138, 1};
+    int iVolumeDimension[3] = { 150, 138, 198 };
     int iComponent = 4;
-    //unsigned char* imageData = LoadRawVolume<unsigned char>(iVolumeDimension, fDoseFilePath);
+    unsigned char* imageData = LoadRawVolume<unsigned char>(iVolumeDimension, fDoseFilePath);
     vtkSmartPointer<vtkImageData> doseImageData = vtkSmartPointer<vtkImageData>::New();
     doseImageData->SetDimensions(iVolumeDimension);
     doseImageData->AllocateScalars(VTK_UNSIGNED_CHAR, iComponent);
+    doseImageData->SetOrigin(100, 100, 0);
+    doseImageData->SetSpacing(2.5, 2.5, 1);
     unsigned char* ptr = reinterpret_cast<unsigned char*>(doseImageData->GetScalarPointer());
-    for (int i = 0; i < iVolumeDimension[0] * iVolumeDimension[1] * iVolumeDimension[2] * iComponent; i+= iComponent)
+    for (int i = 0; i < iVolumeDimension[0] * iVolumeDimension[1] * iVolumeDimension[2] * iComponent; i += iComponent)
     {
-        ptr[i] = 128;
-        ptr[i + 1] = 0;
-        ptr[i + 2] = 0;
-        ptr[i + 3] = 0.5;
+        ptr[i] = imageData[i];
+        ptr[i + 1] = imageData[i + 1];
+        ptr[i + 2] = imageData[i + 2];
+        ptr[i + 3] = imageData[i + 3];
     }
-    //delete imageData;
-
-    // Create a greyscale lookup table
-    vtkSmartPointer<vtkLookupTable> lookupTable = vtkSmartPointer<vtkLookupTable>::New();
-    lookupTable->SetNumberOfColors(4);
-    lookupTable->SetTableRange(0 + 20, 256 - 256 / 4);
-    lookupTable->SetScaleToLinear();
-    lookupTable->SetValueRange(0.1, 1.0);
-    lookupTable->SetHueRange(0.667, 0.0);
-    lookupTable->Build();
-    vtkSmartPointer<vtkImageMapToColors> color =
-        vtkSmartPointer<vtkImageMapToColors>::New();
-    color->SetLookupTable(lookupTable);
-    color->SetInputData(doseImageData);
-    color->Update();
-
-
 
 
     std::string sPath = "E:\\Images\\Test\\";
@@ -225,7 +127,6 @@ int main()
     vtkSmartPointer<vtkMatrix4x4> resliceAxes =
         vtkSmartPointer<vtkMatrix4x4>::New();
     resliceAxes->DeepCopy(axialElements);
-    // Set the point through which to slice
     resliceAxes->SetElement(0, 3, center[0]);
     resliceAxes->SetElement(1, 3, center[1]);
     resliceAxes->SetElement(2, 3, center[2]);
@@ -238,32 +139,43 @@ int main()
     reslice->SetResliceAxes(resliceAxes);
     reslice->SetInterpolationModeToLinear();
 
-    // Create a greyscale lookup table
-    vtkSmartPointer<vtkLookupTable> table =
+    vtkSmartPointer<vtkLookupTable> table2 =
         vtkSmartPointer<vtkLookupTable>::New();
-    table->SetRange(0, 2000); // image intensity range
-    table->SetValueRange(0.0, 1.0); // from black to white
-    table->SetSaturationRange(0.0, 0.0); // no color saturation
-    table->SetRampToLinear();
-    table->Build();
+    table2->SetRange(-355, 300); // image intensity range
+    table2->SetValueRange(0.0, 1.0); // from black to white
+    table2->SetSaturationRange(0.0, 0.0); // no color saturation
+    table2->SetRampToLinear();
+    table2->Build();
 
-    // Map the image through the lookup table
-    vtkSmartPointer<vtkImageMapToColors> color1 =
+
+    vtkSmartPointer<vtkImageMapToColors> map2 =
         vtkSmartPointer<vtkImageMapToColors>::New();
-    color1->SetLookupTable(table);
-    color1->SetInputConnection(reslice->GetOutputPort());
-    color1->Update();
+    map2->SetInputConnection(reslice->GetOutputPort());
+    map2->SetLookupTable(table2);
+    map2->Update();
 
+
+    vtkSmartPointer<vtkLookupTable> table1 =
+        vtkSmartPointer<vtkLookupTable>::New();
+    table1->SetTableRange(0, 1);
+    table1->SetValueRange(1.0, 0.7);
+    table1->SetSaturationRange(0.0, 1.0);
+    table1->SetHueRange(0.12, 0.12);
+    table1->SetAlphaRange(1.0, 1.0);
+    table1->Build();
+
+    vtkSmartPointer<vtkImageMapToColors> map1 =
+        vtkSmartPointer<vtkImageMapToColors>::New();
+    map1->SetInputData(doseImageData);
+    map1->SetLookupTable(table1);
+   map1->Update();
 
     vtkSmartPointer<vtkImageBlend> blend =
         vtkSmartPointer<vtkImageBlend>::New();
-   // blend->SetBlendModeToCompound();
-    blend->AddInputData(doseImageData);
-    blend->AddInputData(reslice->GetOutput());
-    blend->SetOpacity(0,0.5);
+    blend->AddInputData(map2->GetOutput());
+    blend->AddInputData(map1->GetOutput());
+    blend->SetOpacity(0, 0.5);
     blend->SetOpacity(1, 0.5);
-    blend->Update();
-
 
 
     // Display the image
@@ -291,4 +203,3 @@ int main()
 
     return EXIT_SUCCESS;
 }
-
